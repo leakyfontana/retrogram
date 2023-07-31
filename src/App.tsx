21ed7fa72051d476dbd3c9eaecef2e70bda26333
Xander Dyer <xander.dyer@titantechnologies.com>
2023-07-31 00:28:44 -0400

    post list ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { useState } from "react";
 import "./App.css";
 import NavBar from "./components/NavBar";
 import PostGrid from "./components/PostGrid";
 import ProfileHeader from "./components/ProfileHeader";
+import NavButtonEnum from "./helpers/enums/NavButtonEnum";
+import PostList from "./components/PostList";
 
 function App() {
+  const [selectedButton, setSelectedButton] = useState<number>(NavButtonEnum.Grid)
+
   return (
     <div className="bg-[#c9cccf] h-screen flex flex-col items-center justify-between max-w-lg mx-auto rounded-md">
       <header className="bg-gradient-to-b from-[#5A88AF] to-[#214E72] w-full flex flex-row justify-center py-2 items-center rounded-t-md">
@@ -11,8 +16,8 @@ function App() {
       </header>
       <div className="w-full px-3 flex flex-col h-full py-3 overflow-y-scroll">
         <ProfileHeader />
-        <NavBar />
-        <PostGrid />
+        <NavBar selectedButton={selectedButton} setSelectedButton={setSelectedButton} />
+        {selectedButton === NavButtonEnum.Grid ? <PostGrid /> : <PostList />}
       </div>
       {/* <footer className="bg-gradient-to-b from-[#5A5D5B] to-[#4A4B4A] w-full flex flex-row justify-center h-16 items-center rounded-b-md text-white">I am the footer</footer> */}
     </div>
---
+++ src/components/PostListImage.tsx
@@ -0,0 +1,28 @@
+import { FaComment } from 'react-icons/fa';
+import { MdLocationPin } from 'react-icons/md';
+import { PiClockFill } from 'react-icons/pi';
+
+const PostListImage = () => {
+  return (
+    <figure className="m-0 drop-shadow-md bg-[#eeeeee] p-2 rounded-lg flex flex-col">
+      <div className='pb-2 flex flex-row items-center text-sm justify-between pr-1'>
+        <div className='flex flex-row items-center gap-[1px] text-[#5E81C2]'>
+          <MdLocationPin size={16} />
+          <p>Undisclosed Location</p>
+        </div>
+        <div className='flex flex-row items-center gap-[2px] text-[#c9cccf]'>
+          <PiClockFill className='transform -scale-x-100' size={16} />
+          <p>4w</p>
+        </div>
+      </div>
+      <img className="w-full align-top" src="assets/images/profile_picture.jpg" alt="" />
+      <div className="flex flex-nowrap items-center gap-2 pt-2 text-sm">
+        <FaComment className="transform -scale-x-100 flex-shrink-0" color='#c9cccf' size={14} />
+        <p className="text-[#214E72] font-semibold">Xander</p>
+        <p className='flex-grow whitespace-normal'>Lorem ipsum dolor sit amet consectetur.</p>
+      </div>
+    </figure>
+  )
+}
+
+export default PostListImage;
