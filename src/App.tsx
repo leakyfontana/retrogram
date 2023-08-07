f0e3eb8daa9b3b567b171b7b9cc1d2f7dad9ce60
Xander Dyer <xdyer@confirmedapp.com>
2023-08-06 15:24:14 -1000

    post list scroll on grid image click

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,42 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import NavBar from "./components/NavBar";
 import PostGrid from "./components/PostGrid";
 import ProfileHeader from "./components/ProfileHeader";
 import NavButtonEnum from "./helpers/enums/NavButtonEnum";
 import PostList from "./components/PostList";
+import scrollTo from "./helpers/functions/scrollTo"
 
 function App() {
-  const [selectedButton, setSelectedButton] = useState<number>(NavButtonEnum.Grid)
+  const [selectedButton, setSelectedButton] = useState<number>(NavButtonEnum.Grid);
+  const [listScrollId, setListScrollId] = useState<string | null>(null);
+
+  const scrollToPost = (postId: string) => {
+    setSelectedButton(NavButtonEnum.List);
+    setListScrollId(postId);
+  }
+
+  useEffect(() => {
+    if (listScrollId) {
+      const postList = document.getElementById('post-list');
+      const post = document.getElementById(listScrollId);
+
+      if (!postList || !post) return;
+      console.log(post.offsetTop);
+      scrollTo(postList, post.offsetTop - 100, 600);
+      setListScrollId(null);
+    }
+  }, [listScrollId])
 
   return (
     <div className="bg-[#c9cccf] h-screen flex flex-col items-center justify-between max-w-lg mx-auto">
       <header className="bg-gradient-to-b from-[#5A88AF] to-[#214E72] w-full flex flex-row justify-center py-2 items-center">
         <h1 className="text-4xl text-white font-billabong">Retrogram</h1>
       </header>
-      <div className="w-full px-3 flex flex-col h-full py-3 overflow-y-scroll">
+      <div className="w-full px-3 flex flex-col h-full py-3 overflow-y-scroll" id="post-list">
         <ProfileHeader />
         <NavBar selectedButton={selectedButton} setSelectedButton={setSelectedButton} />
-        {selectedButton === NavButtonEnum.Grid ? <PostGrid /> : <PostList />}
+        {selectedButton === NavButtonEnum.Grid ? <PostGrid setSelectedButton={setSelectedButton} scrollToPost={scrollToPost} /> : <PostList />}
       </div>
       {/* <footer className="bg-gradient-to-b from-[#5A5D5B] to-[#4A4B4A] w-full flex flex-row justify-center h-16 items-center rounded-b-md text-white">I am the footer</footer> */}
     </div>
--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -4,15 +4,15 @@ import PostListImage from "./PostListImage";
 const PostList: FC = () => {
   return (
     <div className="flex flex-col gap-3">
-      <PostListImage />
-      <PostListImage />
-      <PostListImage />
-      <PostListImage />
-      <PostListImage />
-      <PostListImage />
-      <PostListImage />
-      <PostListImage />
-      <PostListImage />
+      <PostListImage id="1" />
+      <PostListImage id="2" />
+      <PostListImage id="3" />
+      <PostListImage id="4" />
+      <PostListImage id="5" />
+      <PostListImage id="6" />
+      <PostListImage id="7" />
+      <PostListImage id="8" />
+      <PostListImage id="9" />
     </div>
   )
 }
