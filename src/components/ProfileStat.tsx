43dc83bfb32cb2ec2d5ddc0ac571956901096073
Xander Dyer <xdyer@confirmedapp.com>
2023-08-13 00:00:52 -0400

    adjust stat font size

--- src/components/ProfileStat.tsx
+++ src/components/ProfileStat.tsx
@@ -8,7 +8,7 @@ interface Props {
 const ProfileStat: FC<Props> = ({ name, count }) => {
   return (
     <div className={`w-full border-b flex flex-col items-center justify-center ${name !== 'photos' && 'border-l-2'}`}>
-      <p className="font-bold text-2xl">{count}</p>
+      <p className="text-xl font-bold">{count}</p>
       <p className="text-xs text-[#A3A3A2]">{name}</p>
     </div>
   );
